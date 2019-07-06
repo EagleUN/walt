a2201420cbe151ccbe5ba219ca9e3b871a8395c2
Diego Niquefa <niquefa.diego@gmail.com>
2019-07-05 21:31:40 -0500

    make home feed protected

--- src/moana/resolvers.js
+++ src/moana/resolvers.js
@@ -6,8 +6,10 @@ const URL = `http://${url}:${port}/${entryPoint}`;
 
 const resolvers = {
 	Query: {
-		homeFeedForUser: (_, { id }) =>
+		homeFeedForUserHack: (_, { id }) =>
 			generalRequest(`${URL}/home/${id}`, 'GET'),
+		homeFeedForUser: (_, { id }, context, info) =>
+			protectedGeneralRequest( id, `${URL}/home/${id}`, 'GET', context, info ),
 		profileFeedForUser: (_, { id }) =>
 			generalRequest(`${URL}/profile/${id}`, 'GET')
 	}

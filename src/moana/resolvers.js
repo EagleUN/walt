a2b6eb2a0f3a227234a4484a4614b957b1f1967a
Diego Niquefa <niquefa.diego@gmail.com>
2019-07-07 13:20:59 -0500

    :'v

--- src/moana/resolvers.js
+++ src/moana/resolvers.js
@@ -8,8 +8,8 @@ const resolvers = {
 	Query: {
 		profileFeedForUser: (_, { id }, context, info) =>
 			protectedGeneralRequest( id, `${URL}/profile/${id}`, 'GET', context, info),
-		homeFeedForUser: (_, { id }, context, info) =>
-			protectedGeneralRequest( id, `${URL}/home/${id}`, 'GET', context, info ),
+		homeFeedForUser: (_, { id }) =>
+			protectedGeneralRequest( id, `${URL}/home/${id}`, 'GET'),
 	}
 };
 

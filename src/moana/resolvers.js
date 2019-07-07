548f869554ba562356f0838b3288aac33f39db63
Diego Niquefa <niquefa.diego@gmail.com>
2019-07-07 13:26:20 -0500

    protect request

--- src/moana/resolvers.js
+++ src/moana/resolvers.js
@@ -7,7 +7,7 @@ const URL = `http://${url}:${port}/${entryPoint}`;
 const resolvers = {
 	Query: {
 		profileFeedForUser: (_, { id }, context, info) =>
-			generalRequest( `${URL}/profile/${id}`, 'GET'),
+			protectedGeneralRequest( id, `${URL}/profile/${id}`, 'GET', context, info),
 		homeFeedForUser: (_, { id }, context, info) =>
 			protectedGeneralRequest( id, `${URL}/home/${id}`, 'GET', context, info ),
 	}

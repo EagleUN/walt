d6d58e232c5d9afe1a30004621ab4e060c3e48b6
Diego Niquefa <niquefa.diego@gmail.com>
2019-07-07 13:04:32 -0500

    protect profile feed for user

--- src/moana/resolvers.js
+++ src/moana/resolvers.js
@@ -8,11 +8,13 @@ const resolvers = {
 	Query: {
 		homeFeedForUserHack: (_, { id }) =>
 			generalRequest( `${URL}/home/${id}`, 'GET'),
+		profileFeedForHack: (_, { id }) =>
+			generalRequest(`${URL}/profile/${id}`, 'GET'),
 
 		homeFeedForUser: (_, { id }, context, info) =>
 			protectedGeneralRequest( id, `${URL}/home/${id}`, 'GET', context, info ),
-		profileFeedForUser: (_, { id }) =>
-			generalRequest(`${URL}/profile/${id}`, 'GET')
+		profileFeedForUser: (_, { id }, context, info) =>
+			protectedGeneralRequest( id, `${URL}/profile/${id}`, 'GET', context, info)
 	}
 };
 
